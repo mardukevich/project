c4616bc4a256d0dfcc0f369df958b8aa63a997b9
mardukevich <math1993@mail.ru>
2024-01-20 19:06:42 +0300

    Add /login handler

--- src/server.ts
+++ src/server.ts
@@ -16,11 +16,21 @@ let users: User[] = [
 
 app.use(express.json());
 
-app.get('/api/users', (req: Request, res: Response) => {
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Process the form data (e.g., validate, save to a database, etc.)
+
+  // Send a response back to the client
+  res.send(`Form submitted: Username - ${username}, Password - ${password}`);
+});
+
+app.get('/users', (req: Request, res: Response) => {
   res.json(users);
 });
 
-app.post('/api/users', (req: Request, res: Response) => {
+app.post('/users', (req: Request, res: Response) => {
   const newUser: User = {
     id: users.length + 1,
     name: req.body.name,
@@ -29,14 +39,14 @@ app.post('/api/users', (req: Request, res: Response) => {
   res.status(201).json(newUser);
 });
 
-app.put('/api/users/:id', (req: Request, res: Response) => {
+app.put('/users/:id', (req: Request, res: Response) => {
   const { id } = req.params;
   const updatedUser: User = req.body;
   users = users.map((user) => (user.id === parseInt(id) ? { ...user, ...updatedUser } : user));
   res.json(users.find((user) => user.id === parseInt(id)));
 });
 
-app.delete('/api/users/:id', (req: Request, res: Response) => {
+app.delete('/users/:id', (req: Request, res: Response) => {
   const { id } = req.params;
   users = users.filter((user) => user.id !== parseInt(id));
   res.sendStatus(204);
