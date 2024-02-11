bbc6c39d02a4a379db1f095b5257f5acce744706
mardukevich <math1993@mail.ru>
2024-02-11 23:15:48 +0300

    move types and function to different direcotry

---
+++ src/common.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+export enum Users {
+  PerPage = 20,
+  MaxCount = 10000
+}
+
+export type UserData = {
+  img: string;
+  name: string;
+  login: string;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+function getAddress(location: any) {
+  const number = location['street']['number'];
+  const street = location['street']['name'];
+  const city = location['city'];
+  const state = location['state'];
+  const country = location['country'];
+  const postCode = location['postCode'];
+  return `${street} ${number}, ${city}, ${state}, ${country}, ${postCode}`
+}
+
+function convertToUserData(data: any[]): UserData[] {
+    return data.map(item =>  ({
+        img: item['picture']['thumbnail'],
+        name: item['name']['first'] + ' ' + item['name']['last'],
+        login: item['login'][ 'username'],
+        address: getAddress(item['location']),
+        email: item['email'],
+        phone: item['phone']
+      }))
+}
+
+function useFetchPage(page: number) {
+  const [data, setData] = React.useState<UserData[]>([])
+  const [error, setError] = React.useState();
+
+  React.useEffect(() => {
+    const url = `https://randomuser.me/api/?page=${page}&results=${Users.PerPage}&seed=abc` +
+    '&inc=picture,name,login,location,phone,email'
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setData(convertToUserData(data.results)))
+      .catch(setError)
+  }, [page])
+
+  return [data, error];
+}
