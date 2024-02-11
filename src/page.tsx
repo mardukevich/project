1c11e8c17ec4a1121527e08fbe14ed238f846f41
mardukevich <math1993@mail.ru>
2024-02-11 20:33:28 +0300

    add Header with fliex

--- src/page.tsx
+++ src/page.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 
 import { ViewSwitch, ViewType } from './components';
-import { Pagination } from 'antd';
+import { Flex, Layout, Pagination, Typography } from 'antd';
+import { Header } from 'antd/es/layout/layout';
+
+const { Title } = Typography;
 
 export function Page() {
   const [view, setView] = React.useState<ViewType>();
 
   return (
-    <>
-      <ViewSwitch onChange={setView} />
+    <Layout>
+      <Header>
+        <Flex justify='space-between' align='center'>
+          <Title level={2} >Список пользователей</Title>
+          <ViewSwitch onChange={setView} />
+        </Flex>
+      </Header>
       <Pagination />
-    </>
+    </Layout>
   )
 }
