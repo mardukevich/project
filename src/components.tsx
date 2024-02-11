a22c5421db38e2e4968bdf879de8c00ce431333f
mardukevich <math1993@mail.ru>
2024-02-11 23:16:30 +0300

    make some difference

--- src/components.tsx
+++ src/components.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from 'react-router-dom';
 
 import {  Radio  } from 'antd';
 
+
 export type ViewType = 'table' | 'card';
 
 export const ViewSwitch: React.FC<{ onChange: (view: ViewType) => void }> = ({ onChange }) => {
@@ -29,54 +30,10 @@ interface ViewProps {
   onClicked: (item: any) => void;
 }
 
-type PaginationType = {
-  img: string;
-  name: string;
-  login: string;
-  address: string;
-  email: string;
-  phone: string;
-}
-
-function getAddress(location: any) {
-  const number = location['street']['number'];
-  const street = location['street']['name'];
-  const city = location['city'];
-  const state = location['state'];
-  const country = location['country'];
-  const postCode = location['postCode'];
-  return `${street} ${number}, ${city}, ${state}, ${country}, ${postCode}`
-}
-
-function convertToPaginationType(data: any[]): PaginationType[] {
-    return data.map(item =>  ({
-        img: item['picture']['thumbnail'],
-        name: item['name']['first'] + ' ' + item['name']['last'],
-        login: item['login'][ 'username'],
-        address: getAddress(item['location']),
-        email: item['email'],
-        phone: item['phone']
-      }))
-}
-
-function usePaginationData(page: number): PaginationType[] {
-  const [data, setData] = React.useState<PaginationType[]>([])
-
-  React.useEffect(() => {
-    const url = `https://randomuser.me/api/?page=${page}&results=20&seed=abc` +
-    '&inc=picture,name,login,location,phone,email'
-    fetch(url)
-      .then(response => response.json())
-      .then(data => setData(convertToPaginationType(data.results)))
-  }, [page])
-
-  return data;
-}
-
 export const View: React.FC<ViewProps> = (props) => {
-  const [searchParams, setSearchParams] = useSearchParams({ tab: 'table' });
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const data = usePaginationData(+(searchParams.get('page') ?? 0)) 
+  const [data, error] = useFetchPage(+(searchParams.get('page') ?? 0)) 
 
   return <>
     {data}
