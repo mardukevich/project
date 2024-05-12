f9650dd1eb70224839cf44fa74a30ee015cea145
mardukevich <math1993@mail.ru>
2024-05-12 18:28:26 +0300

    Add interfaces UserInfo and Location, remove using any

--- src/common/common.ts
+++ src/common/common.ts
@@ -2,25 +2,43 @@ import * as React from 'react';
 
 import { UserData, Users } from 'common/types';
 
-function getAddress(location: any) {
-  const number = location['street']['number'];
-  const street = location['street']['name'];
-  const city = location['city'];
-  const state = location['state'];
-  const country = location['country'];
-  const postCode = location['postCode'];
-  return `${street} ${number}, ${city}, ${state}, ${country}, ${postCode}`
+interface UserInfo {
+  picture: { large: string };
+  name: { first: string; last: string };
+  login: { username: string };
+  location: {
+    street: { number: number; name: string };
+    city: string;
+    state: string;
+    country: string;
+    postCode: string;
+  };
+  email: string;
+  phone: string;
 }
 
-function convertToUserData(data: any[]): UserData[] {
-    return data.map(item =>  ({
-        img: item['picture']['large'],
-        name: item['name']['first'] + ' ' + item['name']['last'],
-        login: item['login'][ 'username'],
-        address: getAddress(item['location']),
-        email: item['email'],
-        phone: item['phone']
-      }))
+interface Location {
+  street: { number: number; name: string };
+  city: string;
+  state: string;
+  country: string;
+  postCode: string;
+}
+
+function getAddress(location: Location): string {
+  const { street, city, state, country, postCode } = location;
+  return `${street.name} ${street.number}, ${city}, ${state}, ${country}, ${postCode}`;
+}
+
+function convertToUserData(data: UserInfo[]): UserData[] {
+  return data.map(item => ({
+    img: item.picture.large,
+    name: `${item.name.first} ${item.name.last}`,
+    login: item.login.username,
+    address: getAddress(item.location),
+    email: item.email,
+    phone: item.phone
+  }));
 }
 
 export function useFetchPage(page: number) {
